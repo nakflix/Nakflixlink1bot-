07b061da6560ef805dd9231e4d346c8825b6f092
nakflix <123237114+nakflix@users.noreply.github.com>
2024-02-14 08:21:05 +0300

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -124,7 +124,7 @@ async def not_joined(client: Client, message: Message):
         [
             InlineKeyboardButton(
                 "Join Channel",
-                url = "https://t.me/+HqBWA4vYmMVkNTQ0")
+                url = client.invitelink)
         ]
     ]
     try:
