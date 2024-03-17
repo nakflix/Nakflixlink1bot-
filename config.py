eee12638ce3cadb5b2c7faa9bc15dc3cfd995cd7
nakflix <123237114+nakflix@users.noreply.github.com>
2024-03-17 07:56:59 +0300

    Update config.py

--- config.py
+++ config.py
@@ -49,7 +49,7 @@ except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
 
 #Force sub message 
-FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {first}\n\n<b>You need to join in my Channel to use me\n\nKindly Please join  Channel CLICK HERE 👇 </b>")
+FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {first}\n<b>You Need to be a member of @NAKFLIXTV to use me.\nKindly join @NAKFLIXTV With the Button Below First 👇 </b>")
 
 #set your Custom Caption here, Keep None for Disable Custom Caption
 CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", "POWERED BY @NAKFLIXTV")
@@ -61,7 +61,7 @@ PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" e
 DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'True'
 
 BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
-USER_REPLY_TEXT = "❌Don't send me messages directly I'm only File Share bot Contact @nak_ke!"
+USER_REPLY_TEXT = "❌Don't send me messages directly I'm only File Share bot Contact @nakflixtv!"
 
 ADMINS.append(OWNER_ID)
 ADMINS.append(1458235021)
